775f8db316f215d8b08e4dbe9315e72458893e30
Kaushik Naik <87433036+naik-kaushik@users.noreply.github.com>
2023-07-27 00:04:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ mongoose.connect(process.env.MONGO_URI).then(console.log("DB CONNECTED"));
 
 app.get("/", (req, res) => {
   if (req.isAuthenticated()) {
-    res.status(200).json({message : "OK",user : req.user,extra : req});
+    res.status(200).json({message : "OK",user : req.user});
   } else {
     res.status(401).json({message:"NO"});
   }
